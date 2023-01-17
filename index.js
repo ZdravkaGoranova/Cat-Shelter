74f2bd6b532f8df7a6e61bfb004d7adefe0ca567
Zdravka Goranova <106737347+ZdravkaGoranova@users.noreply.github.com>
2023-01-17 12:50:06 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
 const http = require('http');
+const fs = require('fs/promises');
+const fss = require('fs');
+//const path = require('path');
+const querystring = require('querystring');
 
-const homePage = require('./views');//homePage= require('./views/index.js');
-const siteCss = require('./css/site.css.js');
-const editCat = require('./views/editCat.js');
+
 const cats = require('./cats.json');
 
-const server = http.createServer((req, res) => {
+//Зареждаме страниците от string документ
+// const homePage = require('./viewsOld');//homePage= require('./views/index.js');
+// const siteCss = require('./css/site.css.js');
+// const editCat = require('./viewsOld/editCat.js');
 
+const server = http.createServer(async (req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html',
     });
-
-
     if (req.url == "/") {
-        res.write(homePage);
+        const homePage = await readFile('./views/home.html');
+
+        const catsHtml = cats.map(cat => catTamplate(cat)).join('');
+        const result = homePage.replace('{{cats}}', catsHtml);
+
+        res.write(result);
+
+        //втори вариянт без async
+        // fs.readFile('./views/home/index.html',{encoding:'utf-8'})
+        // .then(homePage=>{res.write(homePage)}).catch(err=>{conole.log(err)});
 
     } else if (/cats\/\d+\/edit/.test(req.url)) {
-        let catId = req.url.split('/')[2];//взимаме номера на котката
-        let cat = cats.find(x => x.id == catId);
-        res.write(editCat(cat));
+        //по-късно ще ни трябват
+        // let catId = req.url.split('/')[2];//взимаме номера на котката
+        // let cat = cats.find(x => x.id == catId);
 
+        let editCatHtml = await readFile('./views/editCat.html');
+        let qsData = querystring.parse(req.url.split('?')[1]);
+        console.log(qsData);
+
+        res.write(editCatHtml);
+
+        // res.write(editCat(cat)); const editCat = require('./viewsOld/editCat.js');
     } else if (req.url == "/css/site.css") {
         res.writeHead(200, {
             'Content-Type': 'text/css',
         });
+        const siteCss = await readFile('./content/style/site.css')
         res.write(siteCss);
 
-    // } else if (req.url == "./views/editCat") {
-    //     res.writeHead(200, {
-    //         'Content-Type': 'text/css',
-    //     });
-    //     res.write(editCat);
+        // } else if (req.url == "./views/editCat") {
+        //     res.writeHead(200, {
+        //         'Content-Type': 'text/css',
+        //     });
+        //     res.write(editCat);
 
     } else {
         res.write(`
@@ -40,8 +61,27 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
-server.listen(5000);
-console.log('Server is running on port 5000 ...');
+function readFile(path) {
+    return fs.readFile(path, { encoding: 'utf-8' });//връща promises
+};
+function catTamplate(cat) {
+    const html = fss.readFileSync('./views/partials/cat.html', { encoding: 'utf-8' });
 
-//С [Ctrl +F5]-стартира автоматично index.js
-// или node .\index.js
+    const modifiedHtml = Object.keys(cat).reduce((result, key) => {
+        return result.replaceAll(`{{${key}}}`, cat[key]);
+    }, html)
+
+    return modifiedHtml;
+
+    //Втори вариянт без reduce
+    // let result = html.replaceAll('{{name}}', cat.name);
+    // result = result.replace('{{breed}}', cat.breed);
+    // result = result.replace('{{imageUrl}}', cat.imageUrl);
+    // result = result.replace('{{description}}', cat.description);
+    //return result;
+};
+
+
+server.listen(5000);
+console.log('Server is running on port 5000...');
+
