5ce71ffc929d0fb3a9fdcbec98102ade9dbddd45
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-01-31 17:47:58 +0200

    setup mongoose

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 const config = {
     production: {
         PORT: 1234,
-     //   DB_URI: 'mongodb://127.0.0.1:27017/cubicle'// база данни
+       DB_URI: 'mongodb://127.0.0.1:27017/catShelter'// база данни
     },
     development: {
         PORT: 5000,
-      //  DB_URI: 'mongodb://127.0.0.1:27017/cubicle'// база данни
+        DB_URI: 'mongodb://127.0.0.1:27017/catShelter'// база данни oбикновенно са различни
     }
 };
 module.exports = config[process.env.node_env || 'development'];
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const express = require('express');
 const routes = require('./routes.js');
 const config = require('./config/index.js');// require('./config');
 const setupViewEngine = require('./config/viewEngine.js');//require('./config/viewEngine.js')(app)
+const initDataBase = require('./config/databaseInit.js')
+
 
 const app = express();//инстанция на нашия сървър
 
@@ -16,6 +18,9 @@ app.use(express.urlencoded({ extended: false }));//връща middleware,кой�
 //прочита данните от req и ще ги парсва за всеки req;
 app.use(routes);//за всички заявки използвай този router
 
-app.listen(config.PORT, () => {
-    console.log(`Server is running on port ${config.PORT}...`)
-});
+
+initDataBase()
+.then(()=>app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)))
+//ако не работи базата данни да не се стартира приложението
+.catch((err)=>console.error(err.message));
+
