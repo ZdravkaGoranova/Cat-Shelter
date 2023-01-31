1538da69a3e09fab029b6089fe97ae16ea226656
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-01-31 19:00:06 +0200

    Save cat to db

--- src/controllers/catController.js
+++ src/controllers/catController.js
@@ -1,17 +1,21 @@
-
-const Cat = require('../models/Cat_old.js');
+const Cat = require('../models/Cat.js');
 
 exports.getCreateCat = (req, res) => {
     res.render('addCat')
 };
 
 exports.postCreateCat = async (req, res) => {
+// let form = new formidable.IncomingForm();
+// let imgUrl='';
+
+
     console.log(req.body)
     res.send('Form submittet')
 
     // //save
-    let cat = new Cat(req.body)
-     await Cat.save(cat);
+    const { name, description, image, breed } = req.body
+    let cat = new Cat({name, description, image, breed})
+    await cat.save();
 
     // //redirect
     res.redirect('/')
